c1b9c4ecdc2a35e7f79aec1869e146e18ff0123c
csabbah <csabbah@ryerson.ca>
2022-12-29 21:47:28 -0500

    Added nodemon as well as a minor update in server/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,14 @@ const app = express();
 const PORT: number = 3003;
 
 app.get("/", (req: Request, res: Response) => {
+  res.setHeader("cookies", "hi");
+  console.log(res.getHeaders());
+
   res.send("Hello World");
 });
 
 app.get("/:id", (req: Request, res: Response) => {
   let param = req.params.id;
-
   res.send(`Endpoint param =${param}`);
 });
 
