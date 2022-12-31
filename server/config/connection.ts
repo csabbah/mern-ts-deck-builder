fd420580b9b31a1b1d58bb72815277e492c0fd57
csabbah <csabbah@ryerson.ca>
2022-12-30 19:05:07 -0500

    Created post method for posting new Deck models & made other minor additions/updates

--- server/config/connection.ts
+++ server/config/connection.ts
@@ -3,8 +3,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URI, () => {
-  console.log("Connected to MongoDB");
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "all-decks",
 });
 
 module.exports = mongoose.connection;
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import Deck from "./models/Deck";
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const PORT: number = 3003;
 
 app.get("/", (req: Request, res: Response) => {
@@ -9,6 +12,20 @@ app.get("/", (req: Request, res: Response) => {
   console.log(res.getHeaders());
 
   res.send("Hello World");
+});
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const title: string = req.body.title;
+
+  // 2 Ways to create a model:
+  // First method
+  //   const newDeck = new Deck({ title: "My awesome deck" });
+  //   const createdDeck = await newDeck.save(); // Save it to the DB
+
+  // Second method
+  const newDeck = await Deck.create({ title });
+
+  res.json(newDeck);
 });
 
 app.get("/:id", (req: Request, res: Response) => {
@@ -20,6 +37,7 @@ const db = require("../config/connection");
 
 // Once DB is open, run the server
 db.once("open", () => {
+  console.log("Connected to MongoDB");
   app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}`);
   });
