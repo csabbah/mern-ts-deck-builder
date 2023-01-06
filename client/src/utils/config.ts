754003ddfc0fead5d22ebd2e47cbe3fb58134614
csabbah <csabbah@ryerson.ca>
2023-01-05 19:40:12 -0500

    Updated API_URL

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,1 +1,5 @@
-export const API_URL = "http://localhost:3003";
+const dev = process.env.NODE_ENV !== "production";
+
+export const API_URL = dev
+  ? "http://localhost:3003"
+  : "https://mern-ts-deck-builder.vercel.app/";
