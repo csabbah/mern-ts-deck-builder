58f3bb82233793b4ff742c82428b52df5f762e64
csabbah <csabbah@ryerson.ca>
2023-01-12 18:51:58 -0500

    Passwords are now hashed when posting users

--- server/src/controllers/userController/createUser.ts
+++ server/src/controllers/userController/createUser.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import User from "../../models/User";
 
+const bcrypt = require("bcryptjs");
+
 export async function createUser(req: Request, res: Response) {
   const username: string = req.body.username;
   const password: string = req.body.password;
+
+  const encryptedPass = await bcrypt.hash(password, 10);
 
   try {
     const isExist = await User.findOne({ username });
@@ -11,7 +15,7 @@ export async function createUser(req: Request, res: Response) {
     if (isExist) {
       return res.status(400).json("User exist!");
     }
-    const newUser = await User.create({ username, password });
+    const newUser = await User.create({ username, password: encryptedPass });
     res.status(200).json(newUser);
   } catch (err) {
     res.status(400).json("Error");
