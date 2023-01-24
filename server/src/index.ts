bcee3530cc321fe2751fce3d3c7ebb1a763ba475
csabbah <csabbah@ryerson.ca>
2023-01-24 11:08:43 -0500

    Minor update made

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,9 +38,9 @@ app.get("/decks/:deckId/:userId", getDeckController);
 app.post("/decks/:deckId/cards", createCardForDeckController);
 app.delete("/decks/:deckId/cards/:cardIndex", deleteCardOnDeckController);
 
-app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"))
 );
 
 const db = require("./config/connection");
