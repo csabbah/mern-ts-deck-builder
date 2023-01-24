7969ec3e491cff7bdb0b8972844c67eeb0ed3d09
csabbah <csabbah@ryerson.ca>
2023-01-24 10:40:48 -0500

    Updated path in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,11 +31,11 @@ const PORT: number = 3003;
 // });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../client/dist")));
+  app.use(express.static(path.join(__dirname, "client/dist")));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
 
 app.get("/decks/:userId", getDecksController);
